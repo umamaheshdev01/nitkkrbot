2c67f00e03dd35a37981df0bbf302ee183b9f402
Uma Mahesh <139320517+umamaheshdev01@users.noreply.github.com>
2024-09-11 23:59:31 +0530

    'ok'

--- app.py
+++ app.py
@@ -1,1 +1,1 @@
-print(1)
+import streamlit as st
---
+++ llm.py
@@ -0,0 +1,11 @@
+import google.generativeai as genai
+genai.configure(api_key="AIzaSyDGJFFUTdcyFzaIcgS698-I7ZvZiWK0WuI")
+
+
+def generate(template,prompt):
+    model = genai.GenerativeModel("gemini-pro")
+    response = model.generate_content(template+prompt)
+    return response.text
+
+
+print(generate('Who is president of ','Pakistan and India'))
